55d02856c335a1b0e29a30f9bf4ef4255b6dec93
Lukas Natanael <lukasnatanael889@gmail.com>
2024-08-12 07:57:24 -0400

    finish exe

--- exe-03-requisições-github/index.js
+++ exe-03-requisições-github/index.js
@@ -1,4 +1,3 @@
-"use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -35,43 +34,120 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
         if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
 };
-Object.defineProperty(exports, "__esModule", { value: true });
-var promptSync = require("prompt-sync");
-var prompt = promptSync();
-var usersList = [];
-var user;
+var users = [];
 function fetchUser(username) {
     return __awaiter(this, void 0, void 0, function () {
-        var response;
+        var response, user;
         return __generator(this, function (_a) {
             switch (_a.label) {
-                case 0: return [4 /*yield*/, fetch("https://api.github.com/users/".concat(username))
-                        .then(function (response) { return response.json(); })
-                        .catch(function (error) { return console.log("Usu\u00E1rio n\u00E3o encontrado! ".concat(error)); })];
+                case 0: return [4 /*yield*/, fetch("https://api.github.com/users/".concat(username))];
                 case 1:
                     response = _a.sent();
-                    if (response.message === 'Not Found') {
+                    return [4 /*yield*/, response.json()];
+                case 2:
+                    user = _a.sent();
+                    if (user.message) {
                         console.log("Usu\u00E1rio n\u00E3o encontrado!");
-                        return [2 /*return*/];
                     }
-                    user = {
-                        id: response.id,
-                        login: response.login,
-                        name: response.name,
-                        bio: response.bio,
-                        public_repos: response.public_repos,
-                        repos_url: response.repos_url,
-                    };
-                    return [2 /*return*/, user];
+                    else {
+                        users.push(user);
+                        console.log("O usu\u00E1rio ".concat(user.login, " foi salvo.\n"), "\nId: ".concat(user.id) +
+                            "\nLogin: ".concat(user.login) +
+                            "\nNome: ".concat(user.name) +
+                            "\nBio ".concat(user.bio) +
+                            "\nReposit\u00F3rios p\u00FAblicos: ".concat(user.public_repos, "\n"));
+                        // showAllUser()
+                        // showRepositoresTotal()
+                        showTopFive();
+                    }
+                    return [2 /*return*/];
             }
         });
     });
 }
-function getUser(username) {
-    // const user = prompt('Informe um username a ser localizado: ')
-    var user = fetchUser(username);
-    usersList.push(user);
+function showUser(username) {
+    return __awaiter(this, void 0, void 0, function () {
+        var user, response, respositories, message_1;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    user = users.find(function (user) { return user.login === username; });
+                    if (!(typeof user === 'undefined')) return [3 /*break*/, 1];
+                    console.log("Usu\u00E1rio ".concat(username, " n\u00E3o encontrado!"));
+                    return [3 /*break*/, 4];
+                case 1: return [4 /*yield*/, fetch(user.repos_url)];
+                case 2:
+                    response = _a.sent();
+                    return [4 /*yield*/, response.json()];
+                case 3:
+                    respositories = _a.sent();
+                    message_1 = "\nId: ".concat(user.id) +
+                        "\nLogin: ".concat(user.login) +
+                        "\nNome: ".concat(user.name) +
+                        "\nBio ".concat(user.bio) +
+                        "\nReposit\u00F3rios p\u00FAblicos: ".concat(user.public_repos, "\n");
+                    respositories.forEach(function (repository) {
+                        message_1 +=
+                            "\nNome: ".concat(repository.name) +
+                                "\nDescri\u00E7\u00E3o: ".concat(repository.description) +
+                                "\nEstrelas: ".concat(repository.stargazers_count) +
+                                "\n\u00C9 um fork: ".concat(repository.fork ? 'Sim' : 'Não', "\n");
+                    });
+                    console.log(message_1);
+                    _a.label = 4;
+                case 4: return [2 /*return*/];
+            }
+        });
+    });
 }
-console.clear();
-getUser('LukasNatanael');
-console.log(usersList);
+function showAllUsers() {
+    var message = '\nUsuários:\n';
+    users.forEach(function (user) { return message += "    - ".concat(user.login, "\n"); });
+    console.log(message);
+}
+function showRepositoresTotal() {
+    var totalRepositories = users.reduce(function (accumulator, user) { return accumulator + user.public_repos; }, 0);
+    console.log("O grupo possui um total de ".concat(totalRepositories, " reposit\u00F3rios p\u00FAblicos!\n"));
+}
+function showTopFive() {
+    var topFive = users.slice().sort(function (currentUser, nextUser) { return nextUser.public_repos - currentUser.public_repos; }).slice(0, 5);
+    var message = 'Top 5 usuários com mais repositórios:\n';
+    topFive.forEach(function (user, index) { return message += "  ".concat(index + 1, " - ").concat(user.login, ": ").concat(user.public_repos, " reposit\u00F3rios.\n"); });
+    console.log(message);
+}
+function main() {
+    return __awaiter(this, void 0, void 0, function () {
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    console.clear();
+                    return [4 /*yield*/, fetchUser('LukasNatanael')];
+                case 1:
+                    _a.sent();
+                    return [4 /*yield*/, fetchUser('isaacpontes')];
+                case 2:
+                    _a.sent();
+                    return [4 /*yield*/, fetchUser('julianaconde')];
+                case 3:
+                    _a.sent();
+                    return [4 /*yield*/, fetchUser('pcaldas')];
+                case 4:
+                    _a.sent();
+                    return [4 /*yield*/, fetchUser('lucasqueirogaa')];
+                case 5:
+                    _a.sent();
+                    return [4 /*yield*/, showUser('LukasNatanael')];
+                case 6:
+                    _a.sent();
+                    return [4 /*yield*/, showUser('isaacpontes')];
+                case 7:
+                    _a.sent();
+                    showAllUsers();
+                    showRepositoresTotal();
+                    showTopFive();
+                    return [2 /*return*/];
+            }
+        });
+    });
+}
+main();
--- exe-03-requisições-github/index.ts
+++ exe-03-requisições-github/index.ts
@@ -1,7 +1,4 @@
-import * as promptSync from 'prompt-sync'
-const prompt = promptSync()
-
-interface User {
+interface GithubUserResponse {
     id:           number
     login:        string
     name:         string
@@ -10,36 +7,102 @@ interface User {
     repos_url:    string
 }
 
-let usersList:[] = []
-let user:User
+interface GithubRepositoriesResponse {
+    name: string
+    description: string
+    fork: boolean
+    stargazers_count: number
+}
+
+const users:GithubUserResponse[] = []
 
 async function fetchUser(username:string) {
+    const response  = await fetch(`https://api.github.com/users/${username}`)
+    const user:GithubUserResponse = await response.json()
 
-    const response = await fetch(`https://api.github.com/users/${username}`)
-    .then( response => response.json() )
-    .catch( error => console.log(`Usuário não encontrado! ${error}`) )
-
-    if (response.message === 'Not Found') {
+    if (user.message) {
         console.log(`Usuário não encontrado!`)
-        return
     }
+    else {
+        users.push(user)
+        console.log(
+            `O usuário ${user.login} foi salvo.\n`,
+            `\nId: ${user.id}`      +
+            `\nLogin: ${user.login}` +
+            `\nNome: ${user.name}`  +
+            `\nBio ${user.bio}`     +
+            `\nRepositórios públicos: ${user.public_repos}\n`
+        )
+        // showAllUser()
+        // showRepositoresTotal()
+        showTopFive()
 
-    user = {
-        id: response.id,
-        login: response.login,
-        name: response.name,
-        bio: response.bio,
-        public_repos: response.public_repos,
-        repos_url: response.repos_url,
     }
-    return user
 }
 
-function getUser(username:string) {
-    // const user = prompt('Informe um username a ser localizado: ')
-    const user = fetchUser(username)
-    usersList.push(user)
+async function showUser(username:string) {
+    const user = users.find( user => user.login === username  )
+
+    if (typeof user === 'undefined') {
+        console.log(`Usuário ${username} não encontrado!`)
+    }
+    else {
+        const response = await fetch(user.repos_url)
+        const respositories: GithubRepositoriesResponse[] = await response.json()
+        
+        let message = 
+        `\nId: ${user.id}`      +
+        `\nLogin: ${user.login}` +
+        `\nNome: ${user.name}`  +
+        `\nBio ${user.bio}`     +
+        `\nRepositórios públicos: ${user.public_repos}\n`
+
+        respositories.forEach( repository => {
+            message +=
+            `\nNome: ${repository.name}` +
+            `\nDescrição: ${repository.description}` +
+            `\nEstrelas: ${repository.stargazers_count}` +
+            `\nÉ um fork: ${repository.fork ? 'Sim' : 'Não'}\n`
+        })
+        console.log(message)
+    }
 }
-console.clear()
-getUser('LukasNatanael')
-console.log(usersList)
+
+function showAllUsers() {
+    let message = '\nUsuários:\n'
+    
+    users.forEach( user => message += `    - ${user.login}\n` )
+    console.log(message)
+}
+
+function showRepositoresTotal() {
+    const totalRepositories = users.reduce( (accumulator, user) => accumulator + user.public_repos, 0 )
+    console.log(`O grupo possui um total de ${totalRepositories} repositórios públicos!\n`)
+}
+
+function showTopFive() {
+    const topFive = users.slice().sort( (currentUser, nextUser) => nextUser.public_repos - currentUser.public_repos ).slice(0, 5)
+    let message = 'Top 5 usuários com mais repositórios:\n'
+    topFive.forEach( (user, index) => message += `  ${index + 1} - ${user.login}: ${user.public_repos} repositórios.\n` )
+    
+    console.log(message)
+}
+
+async function main() {
+    console.clear()
+    
+    await fetchUser('LukasNatanael')
+    await fetchUser('isaacpontes')
+    await fetchUser('julianaconde')
+    await fetchUser('pcaldas')
+    await fetchUser('lucasqueirogaa')
+
+    await showUser('LukasNatanael')
+    await showUser('isaacpontes')
+
+    showAllUsers()
+    showRepositoresTotal()
+    showTopFive()
+}
+
+main()
